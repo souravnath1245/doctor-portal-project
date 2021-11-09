494d5fc2dacad3a055ae5d94c4a366650ec3cfab
[Sourav004] <bushonnath1246@gmail.com>
2021-11-09 11:27:29 +0600

    mon kharap

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  // Link
-} from "react-router-dom";
-import Home from "./pages/Home/Home";
+import React from 'react';
 
-function App() {
+function App = () => {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-         {/* <Route path="/about">
-            <About />
-          </Route> */}
-          {/* <Route path="/appointment">
-            < />
-          </Route>  */}
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+    <div>
+      
     </div>
   );
-}
+};
 
 export default App;
